129fb175539a1427437b7c1b5e0893f1496ae93e
Landon Turner <landon@lturner.net>
2024-10-12 10:33:42 -0500

    check for tmux

--- plugin/run-cmd.lua
+++ plugin/run-cmd.lua
@@ -1,4 +1,14 @@
 if vim.fn.has("nvim-0.7.0") ~= 1 then
-	vim.api.nvim_err_writeln("Your plugin requires at least nvim-0.7.0.")
+	vim.api.nvim_err_writeln("run-cmd requires at least nvim-0.7.0.")
+	return
+end
+
+local function is_tmux_available()
+	vim.fn.system("which tmux")
+	return vim.v.shell_error == 0
+end
+
+if not is_tmux_available() then
+	vim.api.nvim_err_writeln("tmux is not installed")
 	return
 end
